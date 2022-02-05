46b4a2aaa3e9a59e66d1c0d4b7cf1c49320530d6
myathtut-yoma <myathtutthu@yomafleet.com>
2022-02-05 11:01:55 +0630

    fix: password check

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Helpers\ResponseTrait;
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -19,10 +17,6 @@ class LoginController extends Controller
     public function login(LoginRequest $request)
     {
         $user = User::user($request->email)->first();
-
-        if (!Hash::check($request->password, $user->password)) {
-            return $this->invalidResponse('Invalid credentials', Response::HTTP_NOT_FOUND);
-        }
 
         return $this->validResponseWithData([
             'user' => $user,
